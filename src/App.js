6e65f811ba44db489a5f4c2a31d7a3a0e2133df5
Aaron Williams <aaronrw4@yahoo.com>
2019-09-18 20:40:50 -0700

    Attempting Stretch

--- src/App.js
+++ src/App.js
@@ -9,13 +9,51 @@ function App() {
   //You'll need one for the home score and another for the away score.
   const [homeScore, homeScoreCount] = React.useState(32);
   const [awayScore, awayScoreCount] = React.useState(32);
+  const [homeName, homeNameChange] = React.useState('Lions');
+  const [awayName, awayNameChange] = React.useState('Tigers');
+
 
   return (
     <div className="container">
+      <div>
+        <form onSubmit={homeNameChange}>
+          <label>
+            Home team's name?
+            <input type='text' name='homeTeam'/>
+          </label>         
+          <input type='submit' value='Submit' onChange={homeName}></input>          
+        </form>
+
+        <form>
+          <label>Away team's name?
+          <input type='text' name='awayTeam'/>
+          </label>
+          <input type='submit' value='Submit'></input>
+        </form>
+      </div>
+
+      <div>
+        <form>
+          <label>
+            Home team's points?
+            <input type='text' name='homePoints'/>
+          </label>
+          <input type='submit' value='Submit'></input>
+        </form>
+
+        <form>
+          <label>
+            Away team's points?
+            <input type='text' name='awayPoints'/>
+          </label>
+          <input type='submit' value='Submit'></input>
+        </form>
+      </div>
+
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{homeName}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic 
             values from our state. */}
@@ -24,7 +62,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{awayName}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
