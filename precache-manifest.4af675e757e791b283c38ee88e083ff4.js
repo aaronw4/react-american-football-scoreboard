1ecfcf42391955bd9354d93f9c5b45f32b920cf0
Aaron Williams <aaronrw4@yahoo.com>
2021-04-16 16:40:36 -0700

    Updates

---
+++ precache-manifest.4af675e757e791b283c38ee88e083ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9be3927f6a7cb3a9ec7144630675a7",
+    "url": "/react-american-football-scoreboard/index.html"
+  },
+  {
+    "revision": "944317ef6d589976d176",
+    "url": "/react-american-football-scoreboard/static/css/main.ff5e140b.chunk.css"
+  },
+  {
+    "revision": "dcdf05b55f333318bf51",
+    "url": "/react-american-football-scoreboard/static/js/2.62c45116.chunk.js"
+  },
+  {
+    "revision": "944317ef6d589976d176",
+    "url": "/react-american-football-scoreboard/static/js/main.f649382d.chunk.js"
+  },
+  {
+    "revision": "8df6c3978f6d0d1e7c2a",
+    "url": "/react-american-football-scoreboard/static/js/runtime~main.e9fbb0de.js"
+  },
+  {
+    "revision": "1abd8c1a58598974d8415573764ba76a",
+    "url": "/react-american-football-scoreboard/static/media/Ozone.1abd8c1a.ttf"
+  }
+]);
